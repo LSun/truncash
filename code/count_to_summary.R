18992a618f15b3abd248fb209b03620cc011ebbd
Lei Sun <sunl@uchicago.edu>
2018-11-10 19:12:01 -0600

    marginal dist plot

--- code/count_to_summary.R
+++ code/count_to_summary.R
@@ -1,6 +1,15 @@
 ## counts: ngene * nsample RNA-seq count matrix
 ## design: design matrix for group identities of samples
 
+top_genes_index = function (g, X) {
+  return(order(rowSums(X), decreasing = TRUE)[1 : g])
+}
+
+lcpm = function (r) {
+  R = colSums(r)
+  t(log2(((t(r) + 0.5) / (R + 1)) * 10^6))
+}
+
 count_to_summary = function (counts, design) {
   dgecounts = edgeR::calcNormFactors(edgeR::DGEList(counts = counts, group = design[, 2]))
   v = limma::voom(dgecounts, design, plot = FALSE)
